a42b60fb6f6a504f0e0bcd7392172100718f6b4a
Mckoev <mckoev@yandex.ru>
2022-07-24 18:39:14 +0300

    change css

--- src/pages/lists/Lists.tsx
+++ src/pages/lists/Lists.tsx
@@ -1,99 +1,105 @@
-import React from 'react';
+import React from 'react'
+import './lists.css'
 
 function Lists() {
-    return (
-        <div className="page page-tasks">
-            <div className="bg"></div>
-            <div className="overlay"></div>
-            <div className="wrapper">
-                <div className="panel panel-tasklist">
-                    <div className="header">
-                        <div className="title">Shopping list (4)</div>
-                    </div>
-                    <div className="newItem">
-                        <div className="text"><i className="fa fa-plus"></i><span className="title">Add new item</span>
-                        </div>
-                        <input type="text" placeholder="Add new item"/>
-                    </div>
-                    <ul>
-                        <li className="checked">
-                            <div className="check"></div>
-                            <div className="title">Milk</div>
-                        </li>
-                        <li>
-                            <div className="check"></div>
-                            <div className="title">Bread</div>
-                        </li>
-                        <li>
-                            <div className="check"></div>
-                            <div className="title">Cheese</div>
-                        </li>
-                        <li>
-                            <div className="check"></div>
-                            <div className="title">Beer</div>
-                        </li>
-                    </ul>
-                </div>
-                <div className="panel panel-tasklist">
-                    <div className="header">
-                        <div className="title">To-do list (5)</div>
-                    </div>
-                    <div className="newItem">
-                        <div className="text"><i className="fa fa-plus"></i><span className="title">Add new item</span>
-                        </div>
-                        <input type="text" placeholder="Add new item"/>
-                    </div>
-                    <ul>
-                        <li>
-                            <div className="check"></div>
-                            <div className="title">Cleaning</div>
-                        </li>
-                        <li>
-                            <div className="check"></div>
-                            <div className="title">Washing</div>
-                        </li>
-                        <li>
-                            <div className="check"></div>
-                            <div className="title">Shopping</div>
-                        </li>
-                        <li className="checked">
-                            <div className="check"></div>
-                            <div className="title">Pay bills</div>
-                        </li>
-                        <li>
-                            <div className="check"></div>
-                            <div className="title">Dusting</div>
-                        </li>
-                    </ul>
-                </div>
-                <div className="panel panel-tasklist">
-                    <div className="header">
-                        <div className="title">Long-term tasks (3)</div>
-                    </div>
-                    <div className="newItem">
-                        <div className="text"><i className="fa fa-plus"></i><span className="title">Add new item</span>
-                        </div>
-                        <input type="text" placeholder="Add new item"/>
-                    </div>
-                    <ul>
-                        <li>
-                            <div className="check"></div>
-                            <div className="title">Prepare boiler</div>
-                        </li>
-                        <li>
-                            <div className="check"></div>
-                            <div className="title">Buy a sofa</div>
-                        </li>
-                        <li>
-                            <div className="check"></div>
-                            <div className="title">Car service</div>
-                        </li>
-                    </ul>
-                </div>
+  return (
+    <div className="page page-tasks">
+      <div className="bg"></div>
+      <div className="overlay"></div>
+      <div className="wrapper">
+        <div className="panel panel-tasklist">
+          <div className="header">
+            <div className="title">Shopping list (4)</div>
+          </div>
+          <div className="newItem">
+            <div className="text">
+              <i className="fa fa-plus"></i>
+              <span className="title">Add new item</span>
             </div>
+            <input type="text" placeholder="Add new item" />
+          </div>
+          <ul>
+            <li className="checked">
+              <div className="check"></div>
+              <div className="title">Milk</div>
+            </li>
+            <li>
+              <div className="check"></div>
+              <div className="title">Bread</div>
+            </li>
+            <li>
+              <div className="check"></div>
+              <div className="title">Cheese</div>
+            </li>
+            <li>
+              <div className="check"></div>
+              <div className="title">Beer</div>
+            </li>
+          </ul>
         </div>
-    );
+        <div className="panel panel-tasklist">
+          <div className="header">
+            <div className="title">To-do list (5)</div>
+          </div>
+          <div className="newItem">
+            <div className="text">
+              <i className="fa fa-plus"></i>
+              <span className="title">Add new item</span>
+            </div>
+            <input type="text" placeholder="Add new item" />
+          </div>
+          <ul>
+            <li>
+              <div className="check"></div>
+              <div className="title">Cleaning</div>
+            </li>
+            <li>
+              <div className="check"></div>
+              <div className="title">Washing</div>
+            </li>
+            <li>
+              <div className="check"></div>
+              <div className="title">Shopping</div>
+            </li>
+            <li className="checked">
+              <div className="check"></div>
+              <div className="title">Pay bills</div>
+            </li>
+            <li>
+              <div className="check"></div>
+              <div className="title">Dusting</div>
+            </li>
+          </ul>
+        </div>
+        <div className="panel panel-tasklist">
+          <div className="header">
+            <div className="title">Long-term tasks (3)</div>
+          </div>
+          <div className="newItem">
+            <div className="text">
+              <i className="fa fa-plus"></i>
+              <span className="title">Add new item</span>
+            </div>
+            <input type="text" placeholder="Add new item" />
+          </div>
+          <ul>
+            <li>
+              <div className="check"></div>
+              <div className="title">Prepare boiler</div>
+            </li>
+            <li>
+              <div className="check"></div>
+              <div className="title">Buy a sofa</div>
+            </li>
+            <li>
+              <div className="check"></div>
+              <div className="title">Car service</div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  )
 }
 
-
-export default Lists;
+export default Lists
--- src/pages/map/Map.tsx
+++ src/pages/map/Map.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
+import React from 'react'
+import './map.css'
 
 function Map() {
-    return (
-        <div className="page page-map">
-            <div className="bg"></div>
-            <div className="overlay"></div>
-            <div className="title">Traffic map</div>
-            <div className="map"></div>
-        </div>
-    );
+  return (
+    <div className="page page-map">
+      <div className="bg"></div>
+      <div className="overlay"></div>
+      <div className="title">Traffic map</div>
+      <div className="map"></div>
+    </div>
+  )
 }
 
-
-export default Map;
+export default Map
