e94e1e2877289580cf21348da91d36d732ccff1e
Mckoev <mckoev@yandex.ru>
2022-08-29 14:46:07 +0300

    edit calendar

--- src/components/home/PanelTasks.tsx
+++ src/components/home/PanelTasks.tsx
@@ -1,5 +1,6 @@
 import {useState} from 'react'
-import {initShopList, shoppingList} from '../tasks/initialData'
+import {shoppingList} from '../tasks/initialData'
+import {initShopList} from "../../mock/mock";
 
 function PanelTasks() {
     const initialValue = localStorage.getItem(shoppingList)
