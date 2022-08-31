89561ab119771b26b6304f9dcd180b746e3847ea
Mckoev <mckoev@yandex.ru>
2022-08-31 12:33:09 +0300

    edit panelasks component

--- src/components/home/PanelTasks.tsx
+++ src/components/home/PanelTasks.tsx
@@ -1,27 +1,35 @@
 import {useState} from 'react'
 import {shoppingList} from '../tasks/initialData'
 import {initShopList} from "../../mock/mock";
+import {useAppSelector} from "../../reduxToolkit/hooks";
 
 function PanelTasks() {
     const initialValue = localStorage.getItem(shoppingList)
 
     const [list, setList] = useState(initialValue ? JSON.parse(initialValue) : initShopList)
 
-    const listItems = list.map((el, index) => (
+    console.log(list)
+
+
+    const listItems = list.slice(0, 3).map((el, index) => (
         <li key={index} className={el.complete ? 'checked' : ''}>
             <div className="check"></div>
             <div className="title">{el.task}</div>
         </li>
     ))
 
+    const isManyTasks = list.length - list.slice(0, 3).length > 0
+
     return (
         <div className="panel panel-tasks">
             <ul>
                 {listItems}
+                { isManyTasks ?
                 <li className="other">
                     <div className="plus"></div>
-                    <div className="title">... and 15 other tasks</div>
+                     <div className="title">... and {list.length - list.slice(0, 3).length} other tasks</div>
                 </li>
+                    : <></>}
             </ul>
         </div>
     )
