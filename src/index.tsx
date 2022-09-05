c24cfcfe70f42d2e6247963dc6f2a03fdc0cfe72
Mckoev <mckoev@yandex.ru>
2022-09-05 14:29:17 +0300

    fix: edit browser router

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,15 @@
 import './index.css';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import App from './App';
 import { store } from './reduxToolkit/store';
 
-const root = ReactDOM.createRoot(
-    document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
     <Provider store={store}>
-        <BrowserRouter>
+        <HashRouter>
             <App />
-        </BrowserRouter>
+        </HashRouter>
     </Provider>
 );
