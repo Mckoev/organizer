30271ad87d6b6e0cd5a799d5c1c5d1e9151fc0d2
Mckoev <mckoev@yandex.ru>
2022-11-03 02:39:04 +0300

    feat: add mobile version

--- src/api/news/newsApiData.ts
+++ src/api/news/newsApiData.ts
@@ -1,3 +1,3 @@
 export const TOKEN_NEWS = 'pub_9964af15d76f88d4529f7c09174bb86ebe72';
 export const TOKEN_NEWS2 = 'pub_1005881ebe736f2499af4e1f8eb438dece964';
-export const URL_LATEST_NEWS = `https://newsdata.io/api/1/news?apikey=${TOKEN_NEWS}&language=ru`;
+export const URL_LATEST_NEWS = `https://newsdata.io/api/1/news?apikey=${TOKEN_NEWS2}&language=ru`;
--- src/helpers/getSize.ts
+++ src/helpers/getSize.ts
@@ -1,22 +1,42 @@
-import { INews } from 'types/interfaices';
+import {INews} from 'types/interfaices';
 
 export function getSize(index: number, items: INews[]): number {
+    const availableScreenWidth = window.screen.availWidth
     let count = 140;
+    if (availableScreenWidth < 480) {
+        count += 100
+    }
+
+        console.log(items[index].description)
+
     if (items[index].title) {
         if (items[index].title.length > 53) {
             count = count + items[index].title.length - 50;
         }
     }
     if (!items[index].description) {
+        if (availableScreenWidth < 480) {
+            count -= 80
+        }
         return count;
     }
     if (items[index].description.length < 100) {
+
+        if (availableScreenWidth < 480) {
+            count += items[index].description.length * 0.6;
+        }
         count += items[index].description.length * 0.18;
     }
     if (items[index].description.length > 200) {
+        if (availableScreenWidth < 480) {
+            count += items[index].description.length * 0.6;
+        }
         count -= items[index].description.length * 0.15;
     }
     if (items[index].description.length > 350) {
+        if (availableScreenWidth < 480) {
+            count += items[index].description.length * 0.6;
+        }
         count -= items[index].description.length * 0.15;
     }
     return count + items[index].description.length * 0.35;
